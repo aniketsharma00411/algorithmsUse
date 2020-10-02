70a56e9f0d85d2cda9488a9270ea9de77f2c8061
kushjaggi <67220319+kushjaggi@users.noreply.github.com>
2020-10-02 00:50:05 -0700

    Update tower_of_hanoi.cpp
    More efficient , added explanation of Tower of Hanoi puzzle , comments and made easy to comprehend.

--- C++/tower_of_hanoi.cpp
+++ C++/tower_of_hanoi.cpp
@@ -1,22 +1,35 @@
-#include<iostream>
-using namespace std;
-void tower(char a,char b,char c,int n)
-{
-	if(n==1)
-	{
-		printf("%d disk from %c to %c\n",n,a,c);
-		return;
-	}
-	tower(a,c,b,n-1);
-	printf("%d disk from %c to %c\n",n,a,c);
-	tower(b,a,c,n-1);
-}
+EXPLANTION :Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+1) Only one disk can be moved at a time.
+2) Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+3) No disk may be placed on top of a smaller disk.
 
-int main()
-{
-	int n;
-	printf("Enter number of disks:");
-	cin>>n;
-	tower('a','b','c',n);
-	return 0;
-}
+
+// C++ recursive function to  
+// solve tower of hanoi puzzle
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void towerOfHanoi(int n, char from_rod, 
+					char to_rod, char aux_rod) 
+{ 
+	if (n == 1) 
+	{ 
+		cout << "Move disk 1 from rod " << from_rod << 
+							" to rod " << to_rod<<endl; 
+		return; 
+	} 
+	towerOfHanoi(n - 1, from_rod, aux_rod, to_rod); 
+	cout << "Move disk " << n << " from rod " << from_rod << 
+								" to rod " << to_rod << endl; 
+	towerOfHanoi(n - 1, aux_rod, to_rod, from_rod); 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int n = 4; // Number of disks 
+	towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods 
+	return 0; 
+} 
+
+// This is code is contributed by kushjaggi
