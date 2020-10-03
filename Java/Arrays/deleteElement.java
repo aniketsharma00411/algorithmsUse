e617b38e76ebb1232fe259ac45a5b89fc0498201
MahmouMabrok <mahmoud.mabrouk@inovaeg.com>
2020-10-03 22:30:17 +0200

    add classes to codes so it can be used with tests

--- Java/Arrays/deleteElement.java
+++ Java/Arrays/deleteElement.java
@@ -1,11 +1,16 @@
-public static int deleteElement(int[] a, int size, int element) {
+package Array;
+
+public class deleteElement {
+    public static int deleteElement(int[] a, int size, int element) {
         int i;
-        for(i = 0; i < size; i++) {
-            if(a[i] == element) break;
+        for (i = 0; i < size; i++) {
+            if (a[i] == element) break;
         }
-        if(i == size) return size;
-        for(int j = i; j < size - 1; j++) {
+        if (i == size) return size;
+        for (int j = i; j < size - 1; j++) {
             a[j] = a[j + 1];
         }
         return (size - 1);
     }
+
+}
--- Java/Arrays/leftRotateByOne.java
+++
@@ -1,8 +0,0 @@
-public static void leftRotateByOne(int[] a) {
-    int temp = a[0];
-    int n = a.length;
-    for(int i=0;i<n-1;i++) {
-        a[i] = a[i + 1];
-    }
-    a[n - 1] = temp;
-}
