888508c97319be15a12c616f04f2010dae3bc3e0
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-10-05 09:54:56 +0530

    updating branch

--- Java/Arrays/moveZerosToEnd.java
+++ Java/Arrays/moveZerosToEnd.java
@@ -1,15 +1,26 @@
-    public static void swap(int[] a, int i, int j) {
+package Array;
+
+public class MoveZerosToEnd {
+
+    private static void swap(int[] a, int i, int j) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
-    
+
+    /**
+     * util method to move item with zero to end
+     * @param a
+     */
     public static void moveZeroToEnd(int[] a) {
         int count = 0;
-        for(int i = 0; i < a.length; i++) {
-            if(a[i] != 0) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0) {
                 swap(a, i, count);
                 count++;
             }
         }
     }
+
+
+}
