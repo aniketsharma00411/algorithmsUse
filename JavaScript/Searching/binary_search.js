e64f879aba0694c9049dae67b84663f899ed5474
ladyhawke98 <71930484+ladyhawke98@users.noreply.github.com>
2020-10-06 10:03:59 -0300

    Add binary_search.js

---
+++ JavaScript/Searching/binary_search.js
@@ -0,0 +1,17 @@
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]){
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if(arr[middle] === elem){
+        return middle;
+    }
+    return -1;
+}
